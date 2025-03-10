4d640ce2de934e42172f6603536b2e7c926e660d
joshuakod123 <150294439+joshuakod123@users.noreply.github.com>
2025-03-11 00:47:28 +0800

    Delete lib directory

--- lib/community/utils/helpers.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-void showSnackBar(BuildContext context, String message, {bool isError = false}) {
-  final snackBar = SnackBar(
-    content: Text(message),
-    backgroundColor: isError ? Colors.red : Colors.green,
-  );
-  ScaffoldMessenger.of(context).showSnackBar(snackBar);
-}
